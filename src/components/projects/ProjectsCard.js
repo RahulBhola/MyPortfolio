c91be5ef9f48d22f400db521767ececb1e4d535d
RahulBhola <lovebhola8283@gmail.com>
2024-06-02 14:24:44 +0530

    changes in projectCard section

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -7,7 +7,7 @@ const ProjectsCard = ({ title, des, src, projectLink, webLink }) => {
     <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
       <div className="w-full h-[80%] overflow-hidden rounded-lg">
         <img
-          className="h-full object-cover group-hover:scale-110 duration-300 cursor-pointer"
+          className="w-full h-full object-cover group-hover:scale-110 duration-300 cursor-pointer"
           src={src}
           alt="src"
         />
